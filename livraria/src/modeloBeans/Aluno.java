4c389b066dfe30f1ae0d22af48234cf8020a86f4
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:19:56 -0300

    Update Aluno.java

--- livraria/src/modeloBeans/Aluno.java
+++ livraria/src/modeloBeans/Aluno.java
@@ -1,59 +1,72 @@
 package modeloBeans;
 
-public class Aluno {
+public class Aluno
+{
 
-   public String getPesquisa() {
-      return pesquisa;
-   }
+    public String getPesquisa()
+    {
+        return pesquisa;
+    }
 
-   public void setPesquisa(String pesquisa) {
-      this.pesquisa = pesquisa;
-   }
+    public void setPesquisa(String pesquisa)
+    {
+        this.pesquisa = pesquisa;
+    }
 
-   public Integer getNumero_ra() {
-      return numero_ra;
-   }
+    public Integer getNumero_ra()
+    {
+        return numero_ra;
+    }
 
-   public void setNumero_ra(Integer numero_ra) {
-      this.numero_ra = numero_ra;
-   }
+    public void setNumero_ra(Integer numero_ra)
+    {
+        this.numero_ra = numero_ra;
+    }
 
-   public Integer getCodigo() {
-      return codigo;
-   }
+    public Integer getCodigo()
+    {
+        return codigo;
+    }
 
-   public void setCodigo(Integer codigo) {
-      this.codigo = codigo;
-   }
+    public void setCodigo(Integer codigo)
+    {
+        this.codigo = codigo;
+    }
 
-   public String getNome() {
-      return nome;
-   }
+    public String getNome()
+    {
+        return nome;
+    }
 
-   public void setNome(String nome) {
-      this.nome = nome;
-   }
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
 
-   public String getCurso() {
-      return curso;
-   }
+    public String getCurso()
+    {
+        return curso;
+    }
 
-   public void setCurso(String curso) {
-      this.curso = curso;
-   }
+    public void setCurso(String curso)
+    {
+        this.curso = curso;
+    }
 
-   public String getEstado() {
-      return estado;
-   }
+    public String getEstado()
+    {
+        return estado;
+    }
 
-   public void setEstado(String estado) {
-      this.estado = estado;
-   }
+    public void setEstado(String estado)
+    {
+        this.estado = estado;
+    }
 
-   private Integer codigo;
-   private String nome;
-   private String curso;
-   private String estado;
-   private Integer numero_ra;
-   private String pesquisa;
+    private Integer codigo;
+    private String nome;
+    private String curso;
+    private String estado;
+    private Integer numero_ra;
+    private String pesquisa;
 }
